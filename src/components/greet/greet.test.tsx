7e1235123bb13cc7d5148f1d2eceddd9d4f36a9e
bhaveshjoshisuperheronot <bhaveshjoshi011@gmail.com>
2024-01-10 04:20:48 +0000

    End of section 2

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -3,14 +3,34 @@
 import { render, screen } from "@testing-library/react"
 import Greet from "./greet"
 
-test('Greet renders correctly', () => {
-    render(<Greet />)
-    const textElement = screen.getByText(/hello/i)
-    expect(textElement).toBeInTheDocument()
+describe('Greet', () => {
+    test('renders correctly', () => {
+        render(<Greet />)
+        const textElement = screen.getByText(/hello/i)
+        expect(textElement).toBeInTheDocument()
+    })
+    
+    test('renders a name', () => {
+        render(<Greet name='Bhavesh' />)
+        const textElement = screen.getByText('Hello Bhavesh')
+        expect(textElement).toBeInTheDocument()
+    })
 })
 
-test('Greet renders with a name', () => {
-    render(<Greet name='Bhavesh' />)
-    const textElement = screen.getByText('Hello Bhavesh')
-    expect(textElement).toBeInTheDocument()
-})
+/*
+
+For filtering
+p - filter by filename
+t - filter by test name
+test.only - to only run this test
+test.skip - to skip this test
+
+it instead test
+fit - test only
+xit - test skip
+
+Tests are ran for :
+__tests__
+greet.spec.tsx
+greet.test.tsx
+*/
--- src/components/greet/greet.tsx
+++ src/components/greet/greet.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-
-type GreetProps = {
-    name?: string
-}
+import { GreetProps } from './greet.types'
 
 const Greet = (props: GreetProps) => {
   return (
-    <div>Hello {props.name}</div>
+    <div>Hello {props.name? props.name:'Guest'}</div>
   )
 }
 
