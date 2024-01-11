6f77fd3a801a321d9a440ce34e801895e8c5df16
bhaveshjoshisuperheronot <bhaveshjoshi011@gmail.com>
2024-01-11 09:23:22 +0000

    user-event interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Application } from './components/application/application';
-import { Skills } from './components/skills/skills';
+// import { Application } from './components/application/application';
+// import { Skills } from './components/skills/skills';
+import { Counter } from './components/counter/counter';
 
 function App() {
   return (
     <div className="App">
-      <Application />
-      <Skills skills={["HTML", "CSS"]} />
+      {/* <Application /> */}
+      {/* <Skills skills={["HTML", "CSS"]} /> */}
+      <Counter />
     </div>
   );
 }
